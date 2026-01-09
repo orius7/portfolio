8a09d48e681e9622b7e43433917851bfdb713583
Aidan Lei <aidanlei@gmail.com>
2026-01-08 20:14:35 -0800

    deploying to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/personal_portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
