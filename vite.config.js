bf7f12f4a9abcd8b7f3ffb6de27b959f05e8fda6
Aidan Lei <aidanlei@gmail.com>
2026-01-08 20:20:00 -0800

    fix repo name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal_portfolio/",
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
